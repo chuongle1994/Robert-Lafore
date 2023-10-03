02510033193672779a93f99623250ccca5bcd88a
Chuong <chuonglepro1994@gmail.com>
2023-10-02 23:27:24 -0400

    caculate the area

--- Chapter-4/3.cpp
+++ Chapter-4/3.cpp
@@ -19,17 +19,15 @@ struct Distance{
 };
 
 struct Volume{
-	Distance length, width, height;
+	Distance length, width;
 };
 
 int main(){
-	float len, wid, hei;
-	Volume room1 = {{13, 2.5}, {11, 5.5}, {5,2.5}};
-
+	float len, wid;
+	Volume room1 = {{13, 2.5}, {11, 5.5} };
 	len = room1.length.feet+ room1.length.inch/12;
 	wid = room1.width.feet+ room1.width.inch/12;
-	hei = room1.height.feet+ room1.height.inch/12;
 	
-	cout << "Volume = " << len * wid * hei << " cubic feet" << endl;
+	cout << "Area = " << len * wid << " square feet" << endl;
 	return 0;
 }
