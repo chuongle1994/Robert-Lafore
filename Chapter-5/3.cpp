453ac8abe866b274bc2843f115fbdc275ba9ca95
Chuong <chuonglepro1994@gmail.com>
2023-10-03 11:10:32 -0400

    passing  by reference

--- Chapter-5/3.cpp
+++ Chapter-5/3.cpp
@@ -13,20 +13,18 @@ void Smaller(int& x, int& y);
 
 int main(){
     
-    int a = 3, b = 8, c = 10, d = 12;
+    int a = 3, b = 8;
 
     Smaller(a, b);
-    Smaller(c, d);
 
-    cout << endl << "a = " << a << " b = " << b << endl;
-    cout << "c = " << c << " d = " << d << endl;
+    cout  << "The smaller number is: " << a << endl;
     
     return 0;
 }
 
 void Smaller(int& x, int& y){
     if(x < y)
-        x = 0;
+        x = x;
     else
-        y = 0;    
+        y = y;    
 }
