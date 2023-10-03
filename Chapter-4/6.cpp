2abd2dbd6856fe7432d3d98901b91cc2d70ce1ea
Chuong <chuonglepro1994@gmail.com>
2023-10-02 23:34:54 -0400

    enum is a set of related name

--- Chapter-4/6.cpp
+++ Chapter-4/6.cpp
@@ -9,6 +9,7 @@ enum etype , and finally displays the complete word
 #include<iostream>
 
 using namespace std;
+//to define relative name
 
 enum etype {laborer, secretary, manager, accountant, executive, researcher};
 
@@ -18,38 +19,29 @@ int main(){
     cout << "Enter employee type (first letter only laborer, secretary, manager, accountant, executive, researcher): ";
     cin >> type;
     switch (type){
-        case 'l': etype1 = laborer; break;
-        case 's': etype1 = secretary; break;
-        case 'm': etype1 = manager; break;
-        case 'a': etype1 = accountant; break;
-        case 'e': etype1 = executive; break;
-        case 'r': etype1 = researcher; break;
-        default:
-            break;
-    }
-    switch(etype1){
-        case laborer:
-            cout << "Employee type is laborer";
-            break;
-        case secretary:
-            cout << "Employee type is secretary";
-            break;
-        case manager:
-            cout << "Employee type is manager";
-            break;
-        case accountant:
-            cout << "Employee type is accountant";
-            break;
-        case executive:
-            cout << "Employee type is executive";
-            break;
-        case researcher:
-            cout << "Employee type is researcher";
-            break;
+        case 'l': etype1 = laborer;
+        cout <<"Employee type is laborer";
+        break;
+        case 's': etype1 = secretary; 
+        cout << "Employee type is secretary";
+        break;
+        case 'm': etype1 = manager; 
+        cout << "Employee type is manager";
+        break;
+        case 'a': etype1 = accountant; 
+        cout << "Employee type is accountant";
+        break;
+        case 'e': etype1 = executive; 
+        cout << "Employee type is executive";
+        break;
+        case 'r': etype1 = researcher;
+        cout << "Employee type is researcher";
+        break;
         default:
             cout << "Invalid input";
             break;
     }
+
     cout << endl;
     return 0;
 }
